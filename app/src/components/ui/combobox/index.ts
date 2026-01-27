8d6aa40f4f593e8796d12177dd21b427f4fae372
Adel Khial <adel.khial@gmail.com>
2026-01-27 15:45:55 +0100

    chore: configure eslint

---
+++ app/src/components/ui/combobox/index.ts
@@ -0,0 +1,18 @@
+export {
+  Combobox,
+  ComboboxInput,
+  ComboboxContent,
+  ComboboxList,
+  ComboboxItem,
+  ComboboxGroup,
+  ComboboxLabel,
+  ComboboxCollection,
+  ComboboxEmpty,
+  ComboboxSeparator,
+  ComboboxChips,
+  ComboboxChip,
+  ComboboxChipsInput,
+  ComboboxTrigger,
+  ComboboxValue,
+} from "./combobox";
+export { useComboboxAnchor } from "./use-combobox-anchor";
